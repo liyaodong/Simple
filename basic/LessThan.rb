41e076d1df865505bda0c216a84ebc34e18fc6c8
Yaodong Li <mail@liyaodong.com>
2018-10-31 21:38:22 +0800

    Add boolean and less than

---
+++ basic/LessThan.rb
@@ -0,0 +1,24 @@
+require './Boolean.rb'
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      LessThan.new(left.reduce, right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce)
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+
+  def inspect
+    "-#{self}-"
+  end
+end
