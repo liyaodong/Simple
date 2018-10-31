e7d1e0f7544b5d6fc891b0f3cdfbf74b90a32e61
Yaodong Li <mail@liyaodong.com>
2018-10-31 21:08:39 +0800

    Add basic reduce rule

--- basic/Multiply.rb
+++ basic/Multiply.rb
@@ -1,4 +1,18 @@
 class Multiply < Struct.new(:left, :right)
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+
   def to_s
     "#{left} * #{right}"
   end
--- basic/Number.rb
+++ basic/Number.rb
@@ -1,4 +1,8 @@
 class Number < Struct.new(:value)
+  def reducible?
+    false
+  end
+
   def to_s
     value.to_s
   end
